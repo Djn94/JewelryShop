ad0cc610d26ad539de86b3262120c51fcdb4ddb0
Non Dewman <Dnewman1934@yahoo.com>
2019-08-13 11:58:31 -0500

    delete in master branch

--- jewelryshop/models/bracelet.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (sequelize, DataTypes) {
-    const Bracelets = sequelize.define("Bracelets", {
-        name: { type: DataTypes.STRING, allowNull: false },
-        image: { type: DataTypes.STRING, allowNull: false },
-        desc: { type: DataTypes.STRING, allowNull: false },
-        price: { type: DataTypes.FLOAT, allowNull: false },
-    });
-    return Bracelets;
-};
